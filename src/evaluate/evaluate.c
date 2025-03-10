034d00ec38218d9bee2709c3244c6a5914d58989
Marc Schuurman <mschuurm@f1r3s12.codam.nl>
2025-03-10 16:55:04 +0100

    added the boards for eval.h and eval.c

--- src/evaluate/evaluate.c
+++ src/evaluate/evaluate.c
@@ -3,13 +3,11 @@
 
 static const int piece_value[6] = { 100, 300, 300, 500, 900, 1000000 };
 
-int evaluate(const struct position *pos)
-{
+int evaluate(const struct position *pos) {
 	int score[2] = { 0, 0 };
 	int square;
 
-	for (square = 0; square < 64; square++)
-	{
+	for (square = 0; square < 64; square++) {
 		int piece = pos->board[square];
 
 		if (piece != NO_PIECE) {
@@ -30,30 +28,30 @@ int evaluate(const struct position *pos)
 				else
 					score[color] += bishop_position[63 - square];
 			}
+			else if(type == KNIGHT)
+			{
+				if (color == WHITE)
+					score[color] += knight_position[square];
+				else
+					score[color] += knight_position[63 - square];
+			}
+			else if (type == ROOK)
+			{
+				if (color == WHITE)
+					score[color] += rook_position[square];
+				else
+					score[color] += rook_position[63 - square];
+			
+			}
+			else if (type == QUEEN)
+			{
+				if (color == WHITE)
+					score[color] += queen_position[square];
+				else
+					score[color] += queen_position[63 - square];
+			}
 		}
 	}
 
 	return score[pos->side_to_move] - score[1 - pos->side_to_move];
 }
-
-// static const int pawn_position[64] = {
-// 	 0,  0,  0,  0,  0,  0,  0,  0,
-// 	50, 50, 50, 50, 50, 50, 50, 50,
-// 	10, 10, 20, 30, 30, 20, 10, 10,
-//  	5,  5, 10, 25, 25, 10,  5,  5,
-//  	0,  0,  0, 20, 20,  0,  0,  0,
-//  	5, -5,-10,  0,  0,-10, -5,  5,
-// 	 5, 10, 10,-20,-20, 10, 10,  5,
-//  	0,  0,  0,  0,  0,  0,  0,  0
-// };
-
-// static const int bishop_position[64] = {
-// 	-20,-10,-10,-10,-10,-10,-10,-20,
-// 	-10,  0,  0,  0,  0,  0,  0,-10,
-// 	-10,  0,  5, 10, 10,  5,  0,-10,
-// 	-10,  5,  5, 10, 10,  5,  5,-10,
-// 	-10,  0, 10, 10, 10, 10,  0,-10,
-// 	-10, 10, 10, 10, 10, 10, 10,-10,
-// 	-10,  5,  0,  0,  0,  0,  5,-10,
-// 	-20,-10,-10,-10,-10,-10,-10,-20,
-// };
