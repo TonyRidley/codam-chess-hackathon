7decfeffc8415db67ffa156a8c106d0c5fab0a8c
tridley <tridley@student.codam.nl>
2025-03-11 16:07:56 +0100

    Improved game_phase function efficiency drastically

--- src/evaluate/game_phase.c
+++ src/evaluate/game_phase.c
@@ -3,27 +3,17 @@
 
 int get_game_phase(const struct position *pos)
 {
-	int phase = 24;  // Starting phase value
+	int phase = 24;
 
-	for (int square = 0; square < 64; square++)
-	{
-		int piece = pos->board[square];
-		if (piece != NO_PIECE)
-		{
-			switch (TYPE(piece))
-			{
-				case KNIGHT:
-				case BISHOP:
-					phase -= 1;
-				break;
-				case ROOK:
-					phase -= 2;
-				break;
-				case QUEEN:
-					phase -= 4;
-				break;
-			}
-		}
-	}
+	// Count knights and bishops (1 point each)
+	phase -= __builtin_popcountll(pos->bitboards[WHITE][KNIGHT] | pos->bitboards[BLACK][KNIGHT]);
+	phase -= __builtin_popcountll(pos->bitboards[WHITE][BISHOP] | pos->bitboards[BLACK][BISHOP]);
+
+	// Count rooks (2 points each)
+	phase -= 2 * __builtin_popcountll(pos->bitboards[WHITE][ROOK] | pos->bitboards[BLACK][ROOK]);
+
+	// Count queens (4 points each)
+	phase -= 4 * __builtin_popcountll(pos->bitboards[WHITE][QUEEN] | pos->bitboards[BLACK][QUEEN]);
+
 	return phase;
 }
