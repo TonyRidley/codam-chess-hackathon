0866527489f72f93aa19d1f7082624404027ee27
Dev Sewlia <dsewlia@f1r3s14.codam.nl>
2025-03-11 16:16:08 +0100

    changes in search.c

--- src/search/search.c
+++ src/search/search.c
@@ -1,51 +1,57 @@
 #include "search.h"
 #include "evaluate.h"
 #include "generate.h"
-
 #include <limits.h>
+#include <stdbool.h>
 
 struct search_result minimax(const struct position *pos, int depth, int alpha, int beta)
 {
 	struct search_result result;
 
-	result.score = -1000000;
+	result.score = (depth % 2 == 0) ? INT_MIN : INT_MAX;
 
 	if (depth == 0) {
 		/* we have reached our search depth, so evaluate the position.       */
 		result.score = evaluate(pos);
+		return (result);
 	}
-	else
+	struct move moves[MAX_MOVES];
+	size_t count = generate_legal_moves(pos, moves);
+	bool maxi = (depth % 2 == 0);
+	result.score = (maxi == true) ? INT_MIN : INT_MAX;
+
+	for (size_t index = 0; index < count; index++)
 	{
-		struct move moves[MAX_MOVES];
-		size_t count = generate_legal_moves(pos, moves);
-		size_t index;
-
-		for (index = 0; index < count; index++)
+		struct position copy = *pos;
+		int score;
+		/* do a move, the current player in `copy` is then the opponent, */
+		/* and so when we call minimax we get the score of the opponent. */
+		do_move(&copy, moves[index]);
+		/* minimax is called recursively. this call returns the score of */
+		/* the opponent, so we must negate it to get our score.          */
+		score = minimax(&copy, depth - 1, alpha, beta).score;
+		/* update the best move if we found a better one.                */
+		if (maxi)
 		{
-			struct position copy = *pos;
-			int score;
-
-			/* do a move, the current player in `copy` is then the opponent, */
-			/* and so when we call minimax we get the score of the opponent. */
-			do_move(&copy, moves[index]);
-
-			/* minimax is called recursively. this call returns the score of */
-			/* the opponent, so we must negate it to get our score.          */
-			score = -minimax(&copy, depth - 1, alpha, beta).score;
-
-			/* update the best move if we found a better one.                */
 			if (score > result.score)
 			{
-				result.move = moves[index];
 				result.score = score;
+				result.move = moves[index];
 			}
-			if (score > alpha)
-				alpha = score;
-			if (alpha >= beta)
-				break ;
+			alpha = (score > alpha) ? score : alpha;
 		}
+		else
+		{
+			if (score < result.score)
+			{
+				result.score = score;
+				result.move = moves[index];
+			}
+			beta = (score < beta) ? score : beta;
+		}
+		if (alpha >= beta)
+			break ;
 	}
-
 	return result;
 }
 
